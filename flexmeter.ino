9e06a5100211f53e3733efb47f9a8b505e4ea1bc
Carlos <jchgarro@iskolarngbayan.pup.edu.ph>
2020-08-08 22:03:51 +0800

    Rearrange the main expression of the code

--- flexmeter.ino
+++ flexmeter.ino
@@ -7,8 +7,10 @@ LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 //Initialization of Variables
 float voltage= 0.0;
 float length = 0.0;
+float conversion = 0.0;
 int potentio = A2; //Set the port of the potentiometer input in Analog2
 float len = 19.7; //The initial length of the device
+float lenm = 11.5; //The maximum change that can be made or the length of moving part
 
 
 void setup() {
@@ -22,8 +24,9 @@ lcd.print("Circumference"); //This lcd will display "Circumference"
 
 void loop() {
 //The main expression for the prototype
-voltage= analogRead(potentio)
-length =  voltage*(11.5/1023)+len; 
+voltage= analogRead(potentio);
+conversion=(lenm/1023)+len;
+length =  voltage*conversion; 
 
 //As the code run this will display the converted value as 'Length+"cm"' 
 lcd.setCursor(4,1);
@@ -33,6 +36,7 @@ lcd.print(" ");
 lcd.setCursor(11,1);
 lcd.print("cm");
 
+//Show the value in the com port to verify if the LCD is displaying the expected value
 Serial.println(length, 4);
 
 delay(300); //Make delay before make another loop

aa73001767cbc89eb71da736bdfc07aac06507d1
Carlos <jchgarro@iskolarngbayan.pup.edu.ph>
2020-08-08 21:53:01 +0800

    Re-assignment of the variable needed.

--- flexmeter.ino
+++ flexmeter.ino
@@ -1,12 +1,15 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
+//Configuration of the LCD
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+//Initialization of Variables
 float voltage= 0.0;
 float length = 0.0;
+int potentio = A2; //Set the port of the potentiometer input in Analog2
+float len = 19.7; //The initial length of the device
 
-//Set the port of the input in Analog2
-int pot = A2;
 
 void setup() {
 //Intialization of the LCD Screen
@@ -19,8 +22,8 @@ lcd.print("Circumference");
 
 void loop() {
 //The main expression for the prototype
-voltage= analogRead(pot)
-length =  voltage*(11.5/1023)+19.7; 
+voltage= analogRead(potentio)
+length =  voltage*(11.5/1023)+len; 
 
 //As the code run this will display the converted value as "Length cm" 
 lcd.setCursor(4,1);

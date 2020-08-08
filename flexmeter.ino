ab8f848a07a5d1ea6b45aa4325cd3158a9f000fa
Carlos <jchgarro@iskolarngbayan.pup.edu.ph>
2020-08-08 21:33:13 +0800

    Document the lines of the code.

--- flexmeter.ino
+++ flexmeter.ino
@@ -3,25 +3,33 @@
 
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 float voltage= 0.0;
+
+//Set the port of the input in Analog2
 int pot = A2;
 
 void setup() {
-  // put your setup code here, to run once:
+//Intialization of the LCD Screen
 Serial.begin(9600);
+//This lcd will display "Circumference"
 lcd.begin(16,2);
 lcd.setCursor(1,0);
 lcd.print("Circumference");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-voltage =  analogRead(pot)*(11.5/1023)+19.7; //
+//The main expression for the prototype
+voltage =  analogRead(pot)*(11.5/1023)+19.7; 
+
+//As the code run this will display the converted value as "Length cm" 
 lcd.setCursor(4,1);
 lcd.print(voltage, 4);
+
 lcd.print(" ");
 lcd.setCursor(11,1);
+
 lcd.print("cm");
 Serial.println(voltage, 4);
+//Make delay before make another loop
 delay(300);
 
 }
